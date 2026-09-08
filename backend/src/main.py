49737aed1c0e7cb93bf9e87f808dfdf9e721f958
Reshmi-S-2806 <reshmismv@gmail.com>
2026-09-08 08:51:15 +0530

    8th commit

--- backend/src/main.py
+++ backend/src/main.py
@@ -125,7 +125,7 @@ app = FastAPI(title="SnapServe Lead Connect API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://snapserve-instant-callback.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
